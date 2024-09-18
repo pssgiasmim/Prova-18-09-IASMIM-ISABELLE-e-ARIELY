bd681659569df463d0a51812895034f9ecc02b59
ixa532 <belobelle532@gmail.com>
2024-09-18 14:12:31 -0300

    Implementação Método Update

--- Prova 18 09 Iasmim Isabelle e Ariely/Assets/Scripts/Jogador.cs
+++ Prova 18 09 Iasmim Isabelle e Ariely/Assets/Scripts/Jogador.cs
@@ -9,5 +9,39 @@ public class Jogador : MonoBehaviour
     [SerializeField] private Color corDoJogador;
     private Vector2 direcao;
 
+    private void Update()
+    {
+        if (jogador1)
+        {
+            if (Input.GetKey(KeyCode.A))
+                direcao.x = -1;
+            else if (Input.GetKey(KeyCode.D))
+            direcao.x = 1;
+            else direcao.x = 0;
 
+            if (Input.GetKey(KeyCode.W))
+                direcao.y = 1;
+            else if (Input.GetKey(KeyCode.W))
+                direcao.y = 1;
+            else
+                direcao.y = 0;
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+                direcao.x = -1;
+            else if (Input.GetKey(KeyCode.RightArrow))
+                direcao.x = 1;
+            else
+                direcao.x = 0;
+
+            if (Input.GetKey(KeyCode.DownArrow))
+                direcao.y = -1;
+            else if (Input.GetKey(KeyCode.UpArrow))
+                direcao.y = 1;
+            else
+                direcao.y = 0;
+        }
+        transform.Translate(direcao * velocidade * Time.deltaTime);
+    }
 }
