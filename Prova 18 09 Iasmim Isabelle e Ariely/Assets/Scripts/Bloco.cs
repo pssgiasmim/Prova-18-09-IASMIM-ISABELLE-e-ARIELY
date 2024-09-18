bf37dc4c691ad45669509f7d65708370feea3924
ixa532 <belobelle532@gmail.com>
2024-09-18 10:28:50 -0300

    Criação do Método Awake
    Implementação do método

--- Prova 18 09 Iasmim Isabelle e Ariely/Assets/Scripts/Bloco.cs
+++ Prova 18 09 Iasmim Isabelle e Ariely/Assets/Scripts/Bloco.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bloco : MonoBehaviour
@@ -9,4 +10,10 @@ public class Bloco : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private int jogadorDono;
 
+    private void Awake()
+    {
+       spriteRenderer = GetComponent<SpriteRenderer>(); //verificar se falta GameObject
+
+        spriteRenderer.color = Color.white; // Verificar se é preciso utilizar método auxiliar que será criado      
+    }
 }
