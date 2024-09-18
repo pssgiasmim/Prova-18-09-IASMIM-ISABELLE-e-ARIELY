31a58bdf13f7c6203ff594952244e0a6fdc6026f
ixa532 <belobelle532@gmail.com>
2024-09-18 13:45:16 -0300

    Declaração das Variaveis
    Declaração das Variaveis

---
+++ Prova 18 09 Iasmim Isabelle e Ariely/Assets/Scripts/Jogador.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    private const float velocidade = 5f;
+    [SerializeField] private bool jogador1;
+    [SerializeField] private Color corDoJogador;
+    private Vector2 direcao;
+
+
+}
