272a757a35179bba8842c96e2736b702b3197184
pssgiasmim <iasmimpassing@gmail.com>
2024-09-18 10:17:50 -0300

    Declaração de Variáveis de instâncias

---
+++ Prova 18 09 Iasmim Isabelle e Ariely/Assets/Script/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject blockPrefab;
+    [SerializeField] GameObject playerPrefab1;
+    [SerializeField] GameObject playerPrefab2;
+
+    public int linha = 5;
+    public int coluna = 5;
+
+    public float spacing = 1.1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
