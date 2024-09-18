a29eb4a3d11300cf48d7254bc2113863fb7587b0
ixa532 <belobelle532@gmail.com>
2024-09-18 10:14:45 -0300

    Criação de Variaveis (Bloco)
    Criação das variaveis do script Bloco

---
+++ Prova 18 09 Iasmim Isabelle e Ariely/Assets/Scripts/Bloco.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloco : MonoBehaviour
+{
+
+    private bool conquistado = false;
+    private SpriteRenderer spriteRenderer;
+    private int jogadorDono;
+
+}
